9f0292265297f3fddac226abca95f14095b32dfa
gcelesteok <gcelesteok@gmail.com>
2022-10-24 21:38:38 -0300

    actualizacion -idioma

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,11 @@
+var check=document.querySelector(".check")
+check.addEventListener('click',idioma)
 
-const hamburguer = document.querySelector('.hamburguer')
-const menu = document.querySelector('.menu-navegacion')
-
-
-
-hamburguer.addEventListener('click', ()=>{
-    menu.classList.toggle("spread")
-})
-
-window.addEventListener('click', e =>{
-    if(menu.classList.contains('spread') 
-        && e.target != menu && e.target != hamburguer){
-        console.log('cerrar')
-        menu.classList.toggle("spread")
+function idioma(){
+    let id=check.checked;
+    if(id==true){
+        location.href="en/index.html";
+    }else{
+        location.href="../index.html";
     }
-})
+}
